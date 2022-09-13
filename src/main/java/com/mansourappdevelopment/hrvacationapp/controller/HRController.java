0fc6aa1ef67e9d653f04c9b6bf004b169527e3ef
xMansour <MEMansour96@gmail.com>
2022-09-13 15:09:34 +0200

    HR can get employee by id now

--- src/main/java/com/mansourappdevelopment/hrvacationapp/controller/HRController.java
+++ src/main/java/com/mansourappdevelopment/hrvacationapp/controller/HRController.java
@@ -7,6 +7,7 @@ import com.mansourappdevelopment.hrvacationapp.util.DBConnectionManager;
 import com.mansourappdevelopment.hrvacationapp.util.DBManager;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -17,6 +18,11 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class HRController implements Initializable {
@@ -25,15 +31,14 @@ public class HRController implements Initializable {
     @FXML
     private Button getEmployeeBtn;
     @FXML
-    private TextField employeeFirstNameTextFiled;
+    private TextField employeeFirstNameTextField;
     @FXML
-    private TextField employeeLastNameTextFiled;
+    private TextField employeeLastNameTextField;
     @FXML
-    private TextField employeeAnnualVacationTextFiled;
+    private TextField employeeAnnualVacationTextField;
     @FXML
-    private TextField employeeSickVacationTextFiled;
-    @FXML
-    private Button createEmployeeBtn;
+    private TextField employeeSickVacationTextField;
+
     @FXML
     private TableView<Employee> employeesTable;
     @FXML
@@ -47,6 +52,9 @@ public class HRController implements Initializable {
     @FXML
     private TableColumn<Employee, Integer> sickVacationCountCol;
 
+    private HRDAO hrdao;
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
@@ -57,12 +65,54 @@ public class HRController implements Initializable {
 
         DBConnectionManager dbConnectionManager = new DBConnectionManager("config.properties");
         Connection connection = dbConnectionManager.getConnection();
-        DBManager dbManager = new DBManager(connection);
-        dbManager.setUpDatabase();
-        HRDAO hrdao = new HRDAO(connection);
-        ObservableList<Employee> employees = FXCollections.observableArrayList(hrdao.getAllEmployees());
-        employees.forEach(System.out::println);
-        employeesTable.setItems(employees);
+        if (!tableExists(connection)) {
+            DBManager dbManager = new DBManager(connection);
+            dbManager.setUpDatabase();
+        }
+        hrdao = new HRDAO(connection);
+        updateEmployeesTable(hrdao.getAllEmployees());
 
+    }
+
+    public void createNewEmployee(ActionEvent event) {
+        String firstName = employeeFirstNameTextField.getText();
+        String lastName = employeeLastNameTextField.getText();
+        int annualVacationCount = Integer.parseInt(employeeAnnualVacationTextField.getText());
+        int sickVacationCount = Integer.parseInt(employeeSickVacationTextField.getText());
+        hrdao.createNewEmployee(new Employee(null, firstName, lastName, annualVacationCount, sickVacationCount));
+        updateEmployeesTable(hrdao.getAllEmployees());
+    }
+
+    private void updateEmployeesTable(List<Employee> employeeList) {
+        ObservableList<Employee> employees = FXCollections.observableArrayList(employeeList);
+        employeesTable.setItems(employees);
+    }
+
+    private boolean tableExists(Connection connection) {
+        try {
+            DatabaseMetaData databaseMetaData = connection.getMetaData();
+            ResultSet resultSet = databaseMetaData.getTables(null, null, "EMPLOYEES",
+                    new String[]{"TABLE"});
+            resultSet.next();
+            if (resultSet.getString("TABLE_NAME").equals("EMPLOYEES"))
+                return true;
+            else
+                return false;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void getEmployeeById(ActionEvent event) {
+        ArrayList<Employee> emp = null;
+        if (employeeIdTextField.getText().equals("")) {
+            updateEmployeesTable(hrdao.getAllEmployees());
+        } else {
+            emp = new ArrayList<>();
+            emp.add(hrdao.findEmployeeById(Integer.parseInt(employeeIdTextField.getText())));
+            updateEmployeesTable(emp);
+        }
     }
 }
--- src/main/java/com/mansourappdevelopment/hrvacationapp/util/DBManager.java
+++ src/main/java/com/mansourappdevelopment/hrvacationapp/util/DBManager.java
@@ -1,6 +1,7 @@
 package com.mansourappdevelopment.hrvacationapp.util;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -14,8 +15,8 @@ public class DBManager {
     public void setUpDatabase() {
         try {
             Statement statement = connection.createStatement();
-            statement.executeUpdate("DROP TABLE IF EXISTS EMPLOYEES");
-            statement.executeUpdate("CREATE TABLE EMPLOYEES (ID INTEGER PRIMARY KEY AUTOINCREMENT, FIRST_NAME TEXT," +
+            //statement.executeUpdate("DROP TABLE IF EXISTS EMPLOYEES");
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS EMPLOYEES (ID INTEGER PRIMARY KEY AUTOINCREMENT, FIRST_NAME TEXT," +
                     "LAST_NAME TEXT, ANNUAL_VACATION INTEGER, SICK_VACATION INTEGER)");
             statement.executeUpdate("INSERT INTO EMPLOYEES(FIRST_NAME, LAST_NAME, ANNUAL_VACATION, SICK_VACATION) VALUES('Mahmoud', 'Mansour', 15, 7)");
             statement.executeUpdate("INSERT INTO EMPLOYEES(FIRST_NAME, LAST_NAME, ANNUAL_VACATION, SICK_VACATION) VALUES('Khaled', 'Samir', 15, 7)");
